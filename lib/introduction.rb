53075de8bf867601b6eb742f5c066f26a1587115
Jacqueline Bush <jacqui@stainedglassofmckinney.com>
2019-09-23 22:23:11 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,14 +5,18 @@
 
 
 name = "Jacqui"
+language = "Ruby"
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
 introduction(name)
 
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
-
+introduction_with_languge(name, language)
 
 
 
