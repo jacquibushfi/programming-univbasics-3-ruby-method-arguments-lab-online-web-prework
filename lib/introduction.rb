36882e3778ab1e753d07e72f9bc98449639b1c62
Jacqueline Bush <jacqui@stainedglassofmckinney.com>
2019-09-23 22:28:45 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -17,7 +17,7 @@ end
 
 introduction_with_language(name, language)
 
-def introduction_with_language_optional(name, language)
+def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
